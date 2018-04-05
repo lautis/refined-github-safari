1dd4daf019d3f283d7dee6fc33c7590a45f6b2c9
Ville Lautanala <lautis@gmail.com>
2018-04-05 21:57:46 +0300

    Use local storage for chrome.storage polyfill

--- src/chrome.js
+++ src/chrome.js
@@ -1,10 +1,5 @@
 // Mock chrome.storage API for Safari
-
-const data = {
-  options: { disabledFeatures: "show-recently-pushed-branches" }
-};
-
-const getValue = keys => {
+const getValue = (data, keys) => {
   if (typeof keys === "string") {
     return { [keys]: data[keys] };
   } else if (typeof keys === "array") {
@@ -20,18 +15,56 @@ const getValue = keys => {
   }
 };
 
-const storage = {
-  get(keys, callback) {
-    const value = getValue(keys);
-    if (callback) callback(value);
-    return Promise.resolve(value);
-  },
-
-  set(values, callback) {
-    Object.entries(values).forEach(([key, value]) => (data[key] = value));
-    if (callback) callback();
-    return Promise.resolve();
+const safeParse = data => {
+  try {
+    const parsed = JSON.parse(data) || {};
+    parsed.options ||= {};
+    parsed.options.disabledFeatures ||= "show-recently-pushed-branches";
+    return parsed;
+  } catch {
+    return {};
   }
+};
+
+const storage = prefix => {
+  const localStorageKey = `refined-github:${prefix}`;
+  const readLocalStorage = () =>
+    safeParse(localStorage.getItem(localStorageKey));
+  const writeLocalStorage = data =>
+    localStorage.setItem(localStorageKey, JSON.stringify(data));
+  /*const data = {
+    options: { disabledFeatures: "show-recently-pushed-branches" }
+  };*/
+
+  return {
+    get(keys, callback) {
+      const data = readLocalStorage();
+      const value = getValue(data, keys);
+      if (callback) callback(value);
+      return Promise.resolve(value);
+    },
+
+    set(values, callback) {
+      const data = readLocalStorage();
+      Object.entries(values).forEach(([key, value]) => (data[key] = value));
+      writeLocalStorage(data);
+      if (callback) callback();
+      return Promise.resolve();
+    }
+  };
+};
+
+const changes = (oldValue, newValue) => {
+  return Object.keys(newValue).reduce((diff, key) => {
+    if (oldValue[key] === newValue[key]) {
+      return diff;
+    }
+
+    return {
+      ...diff,
+      [key]: { oldValue: oldValue[key], newValue: newValue[key] }
+    };
+  }, {});
 };
 
 browser = chrome = {
@@ -40,9 +73,19 @@ browser = chrome = {
   },
   storage: {
     onChanged: {
-      addListener(listener) {}
+      addListener(listener) {
+        window.addEventListener("storage", event => {
+          if (event.key.startsWith("refined-github:")) {
+            const namespace = event.key.split(":", 2)[1];
+            listener(
+              changes(JSON.parse(event.oldValue), JSON.parse(event.newValue)),
+              namespace
+            );
+          }
+        });
+      }
     },
-    local: storage,
-    sync: storage
+    local: storage("local"),
+    sync: storage("sync")
   }
 };
