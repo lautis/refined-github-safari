7b867fdfea86926f63123b7b6505fdf0e65291d6
Ville Lautanala <lautis@gmail.com>
2018-04-05 22:59:58 +0300

    Move storage to global page

--- src/chrome.js
+++ src/chrome.js
@@ -1,70 +1,50 @@
 // Mock chrome.storage API for Safari
-const getValue = (data, keys) => {
-  if (typeof keys === "string") {
-    return { [keys]: data[keys] };
-  } else if (typeof keys === "array") {
-    return keys
-      .map(key => [key, data[key]])
-      .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
-  } else if (typeof keys === "object") {
-    return Object.entries(keys)
-      .map(([key, value]) => [key, data[key] || value])
-      .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
-  } else {
-    return { ...data };
+const uniqueId = () =>
+  Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, "")
+    .substr(2, 10);
+
+const storageCalls = new Map();
+const sotrageChangeListeners = [];
+
+const respondToMessage = event => {
+  if (event.name === "get-response") {
+    const resolver = storageCalls.get(event.message.id);
+    if (resolver) {
+      resolver(event.message.value);
+      storageCalls.delete(event.message.id);
+    }
+  } else if (event.name === "storage-change") {
+    storageChangeListeners.forEach(listener =>
+      listener(event.message.changes, event.message.namespace)
+    );
   }
 };
 
-const safeParse = data => {
-  try {
-    const parsed = JSON.parse(data) || {};
-    parsed.options ||= {};
-    parsed.options.disabledFeatures ||= "show-recently-pushed-branches";
-    return parsed;
-  } catch {
-    return {};
-  }
-};
+safari.self.addEventListener("message", respondToMessage, false);
 
-const storage = prefix => {
-  const localStorageKey = `refined-github:${prefix}`;
-  const readLocalStorage = () =>
-    safeParse(localStorage.getItem(localStorageKey));
-  const writeLocalStorage = data =>
-    localStorage.setItem(localStorageKey, JSON.stringify(data));
-  /*const data = {
-    options: { disabledFeatures: "show-recently-pushed-branches" }
-  };*/
-
+const storage = namespace => {
   return {
     get(keys, callback) {
-      const data = readLocalStorage();
-      const value = getValue(data, keys);
-      if (callback) callback(value);
-      return Promise.resolve(value);
+      const id = uniqueId();
+      return new Promise(resolve => {
+        storageCalls.set(id, value => {
+          resolve(value);
+          if (callback) {
+            callback(value);
+          }
+        });
+        safari.self.tab.dispatchMessage("get", { id, keys, namespace });
+      });
     },
 
     set(values, callback) {
-      const data = readLocalStorage();
-      Object.entries(values).forEach(([key, value]) => (data[key] = value));
-      writeLocalStorage(data);
+      safari.self.tab.dispatchMessage("set", { values, namespace });
       if (callback) callback();
       return Promise.resolve();
     }
   };
-};
-
-const changes = (oldValue, newValue) => {
-  return Object.keys(newValue).reduce((diff, key) => {
-    if (oldValue[key] === newValue[key]) {
-      return diff;
-    }
-
-    return {
-      ...diff,
-      [key]: { oldValue: oldValue[key], newValue: newValue[key] }
-    };
-  }, {});
 };
 
 browser = chrome = {
@@ -74,15 +54,7 @@ browser = chrome = {
   storage: {
     onChanged: {
       addListener(listener) {
-        window.addEventListener("storage", event => {
-          if (event.key.startsWith("refined-github:")) {
-            const namespace = event.key.split(":", 2)[1];
-            listener(
-              changes(JSON.parse(event.oldValue), JSON.parse(event.newValue)),
-              namespace
-            );
-          }
-        });
+        storageChangeListeners.push(listener);
       }
     },
     local: storage("local"),
