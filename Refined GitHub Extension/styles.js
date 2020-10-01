d327e455a76a578ad435c6a94c49cc29881b8dd8
Federico <me@fregante.com>
2020-10-01 15:52:06 -0500

    Inject styles before dom-ready

--- Refined GitHub Extension/styles.js
+++ Refined GitHub Extension/styles.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function addStylesheet(name) {
-    const link = document.createElement("link");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = safari.extension.baseURI + name;
-    document.head.appendChild(link);
-  }
+function addStylesheet(name) {
+  const link = document.createElement("link");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = safari.extension.baseURI + name;
+  document.head.appendChild(link);
+}
 
-  addStylesheet("refined-github.css");
-  addStylesheet("safari.css");
-
-}, false);
+addStylesheet("refined-github.css");
+addStylesheet("safari.css");
