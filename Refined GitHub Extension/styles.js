b464fc9677d55f8e9e17b605b86d37cc4c779685
Federico <me@fregante.com>
2020-10-02 15:43:39 -0500

    Use polling

--- Refined GitHub Extension/styles.js
+++ Refined GitHub Extension/styles.js
@@ -1,10 +1,16 @@
-function addStylesheet(name) {
-  const link = document.createElement("link");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = safari.extension.baseURI + name;
-  document.head.appendChild(link);
-}
+ function addStylesheet(name) {
+   const link = document.createElement("link");
+   link.type = "text/css";
+   link.rel = "stylesheet";
+   link.href = safari.extension.baseURI + name;
+   document.head.appendChild(link);
+ }
 
-addStylesheet("refined-github.css");
-addStylesheet("safari.css");
+(function inject () {
+  if (document.head) {
+    addStylesheet("refined-github.css");
+    addStylesheet("safari.css");
+  } else {
+    setTimeout(inject, 100);
+  }
+})();
